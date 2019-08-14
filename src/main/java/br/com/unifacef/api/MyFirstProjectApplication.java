e5e7a091395e8398bea8f845af886a7789a9160e
Miguel Aleixo <miguelgomes2099@hotmail.com>
2019-08-13 22:10:47 -0300

    first commit

---
+++ src/main/java/br/com/unifacef/api/MyFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifacef.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectApplication.class, args);
+	}
+
+}
